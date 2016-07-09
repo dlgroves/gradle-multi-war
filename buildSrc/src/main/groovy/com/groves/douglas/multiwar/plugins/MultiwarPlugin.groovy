30035f776592543f723acc6ffe1da8016d47d93b
Douglas Groves <douglas.groves@ntlworld.com>
2016-07-09 16:36:31 +0100

    Generate configurations for each declared module
    Configurations now generated automatically for each container declared in
    the DSL.

--- buildSrc/src/main/groovy/com/groves/douglas/multiwar/plugins/MultiwarPlugin.groovy
+++ buildSrc/src/main/groovy/com/groves/douglas/multiwar/plugins/MultiwarPlugin.groovy
@@ -14,6 +14,7 @@ class MultiwarPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
+        assert !project.childProjects.isEmpty(): 'Project must declare at least one submodule to be a valid multiwar project'
         project.extensions.create('multiwar', MultiwarExtension)
         def containers = project.multiwar.extensions.containers =
                 project.container(Container)
@@ -23,21 +24,18 @@ class MultiwarPlugin implements Plugin<Project> {
 
     def initialiseTasks(Project project, containers) {
         project.afterEvaluate {
+            addConfigurations(project, containers)
             containers.all { Container container ->
-                if(project.multiwar.verbose){
-                    project.getLogger().quiet("Found container: {}", container.name)
-                }
+                !project.multiwar.verbose ?: project.getLogger().quiet("Creating task assemble-{}-war...", container.name)
                 def task = project.task([type: War], "assemble-${container.name}-war") { myTask ->
-                    from("${project.rootDir}/config/${container.name}"){
+                    from("${project.rootDir}/config/${container.name}") {
                         into 'WEB-INF'
                     }
                     classpath project.configurations[container.name]
                     classifier = container.name
                     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
                     doLast {
-                        if(project.multiwar.verbose){
-                            project.getLogger().quiet("Assembling {} war file...", container.name)
-                        }
+                        !project.multiwar.verbose ?: !project.getLogger().quiet("Assembling {} war file...", container.name)
                     }
                 }
                 project.tasks.assemble.dependsOn(task)
@@ -54,6 +52,16 @@ class MultiwarPlugin implements Plugin<Project> {
                     task.enabled = false
                 }
             }
+        }
+    }
+
+    def addConfigurations(Project project, containers) {
+        containers.all { Container container ->
+            assert project.childProjects.get(container.name) : "Module ${container.name} doesn't exist"
+            assert project.file("${project.rootDir}/config/${container.name}").exists(): "Configuration directory ${project.rootDir}/config/${container.name} doesn't exist"
+            !project.multiwar.verbose ?: project.getLogger().quiet("Configuring ${container.name}...")
+            project.configurations.create(container.name).setTransitive(true).setVisible(true)
+            project.dependencies.add(container.name, project.childProjects.get(container.name))
         }
     }
 }
